97e8df1b590f7619a3985ff4d7583e166fdea65e
adozaraz <minecraft-no.adozaraz@yandex.ru>
2022-07-04 00:46:35 +0400

    Локализация проекта Part 4 + фикс возможности удаления сессии с билетами.

--- src/main/java/com/company/cinemaproject/screen/session/SessionBrowse.java
+++ src/main/java/com/company/cinemaproject/screen/session/SessionBrowse.java
@@ -1,12 +1,15 @@
 package com.company.cinemaproject.screen.session;
 
 import com.company.cinemaproject.app.SessionService;
+import io.jmix.ui.action.list.RemoveAction;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.component.Table;
 import io.jmix.ui.screen.*;
 import com.company.cinemaproject.entity.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Named;
 
 @UiController("Session_.browse")
 @UiDescriptor("session-browse.xml")
@@ -18,11 +21,14 @@ public class SessionBrowse extends StandardLookup<Session> {
     private SessionService sessionService;
     @Autowired
     private GroupTable<Session> sessionsTable;
+    @Named("sessionsTable.remove")
+    private RemoveAction<Session> sessionsTableRemove;
 
     @Subscribe("sessionsTable")
     public void onSessionsTableSelection(Table.SelectionEvent<Session> event) {
         if (sessionService.isAnyTicketBought(sessionsTable.getSingleSelected())) {
             removeBtn.setEnabled(false);
+            sessionsTableRemove.setEnabled(false);
         }
     }
 }
